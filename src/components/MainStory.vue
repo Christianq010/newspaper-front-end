b4026bf9c0d3a7a126074fc69b332f177928bc00
Christiaan Quyn <christianq010@gmail.com>
2018-09-13 12:52:56 +0530

    Add MainStory data via .json

--- src/components/MainStory.vue
+++ src/components/MainStory.vue
@@ -1,13 +1,14 @@
 <template>
     <!-- Main Section -->
     <!-- Main Content Wrapper with image-->
-        <div class="main-content-wrapper" v-bind:style="{ 'background-image': 'url(' + image + ')' }">
+        <div class="main-content-wrapper" 
+            v-bind:style="{ 'background-image': 'url(' + image + ')' }">
             <!-- Overlay -->
             <div class="main-content-overlay">
                 <!-- Main Heading -->
                 <div class="main-content-h1-w">
                     <div class="main-content-h1">
-                        {headline}
+                        {{dataObject.headline[0].title}}
                     </div>
                 </div>
                 <!-- /end of Main heading -->
@@ -19,51 +20,94 @@
 </template>
 
 <script>
+// image: "https://img.huffingtonpost.com/asset/5b99d9903d00004d00bc14b6.jpeg",
+// headline: "ALGAE MESS SWAMPS RICK SCOTT"
 import data from "./../data.json";
 
 export default {
-    data: function() {
-        return {
-            image: "https://img.huffingtonpost.com/asset/5b99d9903d00004d00bc14b6.jpeg",
-            headline: "ALGAE MESS SWAMPS RICK SCOTT"
-        }
-    }
+  data: function() {
+    return {
+      dataObject: data,
+      image: data.headline[0].poster
+    };
+  }
 };
-
-
 </script>
 
 <style lang="scss">
+
+@import "./../scss/main";
+
 /* Main Section */
-/* Main Section background Image*/
-.main-content-wrapper {
-  grid-row: 2 / 8;
-  grid-column: 1 / -1;
-  /* height: 480px; */
-  /* Styling */
-  background-size: cover;
-  background-position: 50% 50%;
-  height: 480px;
+/* Smartphones (portrait and landscape) ----------- */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+  .main-content-wrapper {
+    grid-row: 2 / 8;
+    grid-column: 1 / -1;
+    /* Styling */
+    background-size: cover;
+    background-position: 50% 50%;
+    width: 100%;
+  }
+  .main-content-overlay {
+    background-color: rgba(0, 0, 0, 0.4);
+    background: linear-gradient(to top, rgba(0, 0, 0, 0.4), transparent 100%);
+  }
+  /* Main Section Headline H1 */
+  .main-content-h1-w {
+    text-align: center;
+  }
+  .main-content-h1 {
+    font-family: "ProximaNovaBold", Arial, Geneva, sans-serif;
+    color: white;
+    font-size: 3rem;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    margin-top: 4.5em;
+  }
+  .main-content-h1:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-decoration-color: $primary-color;
+  }
+
 }
-.main-content-overlay {
-  background-color: rgba(0, 0, 0, 0.4);
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.4), transparent 100%);
-}
-/* Main Section Headline H1 */
-.main-content-h1-w {
-  width: 980px;
-  margin: 0 auto;
-  text-align: center;
-}
-.main-content-h1 {
-  font-family: "ProximaNovaBold", Arial, Geneva, sans-serif;
-  color: white;
-  font-size: 5.125rem;
-  text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-}
-.main-content-h1:hover {
-  cursor: pointer;
-  text-decoration: underline;
-  text-decoration-color: #0dbe98;
+
+/* Desktops and laptops ----------- */
+@media only screen and (min-width: 1224px) {
+  /* Main Section background Image*/
+  .main-content-wrapper {
+    grid-row: 2 / 8;
+    grid-column: 1 / -1;
+    /* height: 480px; */
+    /* Styling */
+    background-size: cover;
+    background-position: 50% 50%;
+    height: 480px;
+  }
+  .main-content-overlay {
+    background-color: rgba(0, 0, 0, 0.4);
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.4),
+      transparent 100%
+    );
+  }
+  /* Main Section Headline H1 */
+  .main-content-h1-w {
+    width: 980px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .main-content-h1 {
+    font-family: "ProximaNovaBold", Arial, Geneva, sans-serif;
+    color: white;
+    font-size: 5.125rem;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  }
+  .main-content-h1:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-decoration-color: $primary-color;
+  }
 }
 </style>
