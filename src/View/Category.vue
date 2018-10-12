8318462bbb733bc285b0f2c26711b7d24b3bcbf2
Christiaan Quyn <christianq010@gmail.com>
2018-10-12 16:24:18 +0530

    Add category component and view

---
+++ src/View/Category.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <navbar />
+        
+        <top-videos />
+        <trending-column />
+        <in-the-news />
+        <footer-section />
+    </div>
+</template>
+
+<script>
+// Navigation Bar
+import Navbar from "./../components/Navbar/Navbar";
+
+import TopVideos from "./../components/TopVideos";
+import TrendingColumn from "./../components/Trending";
+import InTheNews from "./../components/InTheNews";
+
+// Footer
+import FooterSection from "./../components/Footer";
+
+export default {
+name: "categorypage",
+  components: {
+    Navbar,
+    TopVideos,
+    TrendingColumn,
+    InTheNews,
+    FooterSection
+  }
+}
+</script>
+
+<style scoped>
+/* Smartphones (portrait and landscape) ----------- */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+  /* Our  Main Container */
+  #app {
+    height: auto;
+    display: grid;
+  }
+}
+/* Desktops and laptops ----------- */
+@media only screen and (min-width: 1224px) {
+  #app {
+    height: auto;
+    display: grid;
+    grid-gap: 0;
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+    grid-auto-rows: 25%;
+    grid-auto-flow: dense;
+  }
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import Home from './View/HomePage.vue'
 import Article from './View/Article.vue'
+import Category from './View/Category.vue'
 
 export const routes = [
     {path: '', component: Home},
-    {path: '/article', component: Article}
+    {path: '/article', component: Article},
+    {path: '/category', component: Category}
 
 ];
